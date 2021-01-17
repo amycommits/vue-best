0c8b4ae2b38ae159871d99519a258a0dfd14fc73
Amy Detwiler <detwiler.amy@gmail.com>
2021-01-17 13:53:42 -0500

    implemented basic routing to form

---
+++ src/views/Form.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <simple-input />
+  </div>
+</template>
+<script>
+import SimpleInput from '../components/SimpleInput.vue';
+
+export default {
+  name: 'Form',
+  components: {
+    SimpleInput,
+  },
+};
+</script>
